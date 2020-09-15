a14e0079b996d82c32bde3e47893e6d52b9fb40e
Celine Galy <celinegaly63@gmail.com>
2020-09-15 16:56:27 +0200

    maj video.js

--- js/video.js
+++ js/video.js
@@ -5,13 +5,12 @@ let soundOff = document.getElementById('soundOff');
 
 soundOn.addEventListener('click',()=>{
     player.play()
-    player.muted = "true";
     document.getElementById('modale').classList.add('hidden');
 });
 soundOff.addEventListener('click',()=>{
     player.play();
+    player.muted = "true";
     document.getElementById('modale').classList.add('hidden');
-    
 });
 
 // setTimeout(function(){
