08c8657a57bec0ca464c8c6ff17664f27bd268ea
Celine Galy <celinegaly63@gmail.com>
2020-09-23 15:52:05 +0200

    ajout page js

---
+++ js/pseudo.js
@@ -0,0 +1,11 @@
+
+let btnLogin = document.getElementById('btnMenu'); 
+let pseudoPlayerTwo = document.getElementById('pseudoPlayerTwo').value;
+
+btnLogin.addEventListener('click',()=>{
+    let pseudoPlayerOne = document.getElementById('pseudoPlayerOne').value;
+    // Afficher la valeur
+    console.log(pseudoPlayerOne);
+    document.getElementById('PlayerOneDuo').value = pseudoPlayerOne;
+});
+
