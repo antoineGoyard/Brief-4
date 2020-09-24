4666521184c61296270c4c032e7c2758dc382fee
Authier Guillaume <authier.guillaume.pro@gmail.com>
2020-09-24 19:53:53 +0200

    ajout de turn bateau pour tous les types de bateaux

---
+++ js/peopleChoice.js
@@ -0,0 +1,29 @@
+
+var people = localStorage.getItem("people");
+
+if(people == 'republique')
+{
+    document.getElementById('boat-1').setAttribute('src', './img/vaisseau1Bleu.png');
+    document.getElementById('boat-2').setAttribute('src', './img/vaisseau2Bleu.png');
+    document.getElementById('boat-3').setAttribute('src', './img/vaisseau3Bleu.png');
+    document.getElementById('boat-4').setAttribute('src', './img/vaisseau3Bleu.png');
+    document.getElementById('boat-5').setAttribute('src', './img/vaisseau3Bleu.png');
+    console.log('repu');
+}
+else if(people == 'empire')
+{
+    document.getElementById('boat-1').setAttribute('src', './img/vaisseau1Rouge.png');
+    document.getElementById('boat-2').setAttribute('src', './img/vaisseau2Rouge.png');
+    document.getElementById('boat-3').setAttribute('src', './img/vaisseau3Rouge.png');
+    document.getElementById('boat-4').setAttribute('src', './img/vaisseau3Rouge.png');
+    document.getElementById('boat-5').setAttribute('src', './img/vaisseau3Rouge.png');
+}
+else
+{
+    document.getElementById('boat-1').setAttribute('src', './img/vaisseau1Bleu.png');
+    document.getElementById('boat-2').setAttribute('src', './img/vaisseau2Bleu.png');
+    document.getElementById('boat-3').setAttribute('src', './img/vaisseau3Bleu.png');
+    document.getElementById('boat-4').setAttribute('src', './img/vaisseau3Bleu.png');
+    document.getElementById('boat-5').setAttribute('src', './img/vaisseau3Bleu.png');
+    console.log('else');
+}
