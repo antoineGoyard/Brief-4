7e1f03c5137cdb9373b8b89769d4c6fa9fc992b4
antoineGoyard <antoine.goyard@gmail.com>
2020-09-26 11:31:15 +0200

    variable rmv

--- js/randomAI.js
+++ js/randomAI.js
@@ -1,7 +1,3 @@
-let test = true; // si true le dernier coup a touché a touché si false ce coup était raté
-let prochainCoup =0;// Si le dernier coup a toucher cette variable sera le prochain coup a faire pour l'IA
-let x = 2; //niveau de difficulté qui permet de savoir la taille du tableau contenant les bateaux
-let already = [];
 
 /**
  * fonction qui permet a l'IA de jouer un coup aléatoire, de plus elle vérifie si le coup n'a pas déja été joué 
