043383ec4c2ee1afb916b79f68d68321ab89b5c6
Authier Guillaume <authier.guillaume.pro@gmail.com>
2020-09-28 13:47:36 +0200

    Mise a jour DUO

--- js/modeDuo/duoGrille.js
+++ js/modeDuo/duoGrille.js
@@ -31,9 +31,39 @@ for (let u = 0; u < 1; u++)
     }
 }
 
+let varaible100 = 1000
+for (let u = 0; u < 1; u++) 
+{
+    let newItemNew = document.createElement('table');
+    let letableau = 'letableauinvicibleP1';
+    newItemNew.id = (letableau);
+    document.getElementById('game_board_main2P1').appendChild(newItemNew);
+
+    for (let i = 0; i < 10; i++) 
+    {
+        let newItem = document.createElement('tr');
+        newItem.classList.add('uneLigneinvicibleP1');
+        let ligneID = 'uneLigneinvicibleP1' + i;
+        newItem.id = (ligneID);
+        document.getElementById('letableauinvicibleP1').appendChild(newItem);
+
+
+        for (let x = 0; x < 10; x++) 
+        {
+            let itemNew = document.createElement('td');
+            itemNew.classList.add('battlecaseinvicibleP1');
+            itemNew.id = (varaible100);
+            itemNew.classList.add('player1');
+            itemNew.classList.add('dropper');
+            document.getElementById(ligneID).appendChild(itemNew);
+            varaible100++
+        }
+    }
+}
+
 // Grille Player 2
 
-let varaible10 = 1000
+let varaible1000 = 10000
 
 for (let u = 0; u < 1; u++) 
 {
@@ -55,11 +85,41 @@ for (let u = 0; u < 1; u++)
         {
             let itemNew = document.createElement('td');
             itemNew.classList.add('battlecaseP2');
-            itemNew.id = (varaible10);
+            itemNew.id = (varaible1000);
             itemNew.classList.add('player');
             itemNew.classList.add('dropper');
             document.getElementById(ligneID).appendChild(itemNew);
-            varaible1++
+            varaible1000++
+        }
+    }
+}
+let varaible10000 = 100000
+
+for (let u = 0; u < 1; u++) 
+{
+    let newItemNew = document.createElement('table');
+    let letableau = 'letableauP2invicible';
+    newItemNew.id = (letableau);
+    document.getElementById('game_board_main2P2').appendChild(newItemNew);
+
+    for (let i = 0; i < 10; i++) 
+    {
+        let newItem = document.createElement('tr');
+        newItem.classList.add('uneLigneP2invicible');
+        let ligneID = 'uneLigneP2invicible' + i;
+        newItem.id = (ligneID);
+        document.getElementById('letableauP2invicible').appendChild(newItem);
+
+
+        for (let x = 0; x < 10; x++) 
+        {
+            let itemNew = document.createElement('td');
+            itemNew.classList.add('battlecaseP2invicible');
+            itemNew.id = (varaible10000);
+            itemNew.classList.add('player');
+            itemNew.classList.add('dropper');
+            document.getElementById(ligneID).appendChild(itemNew);
+            varaible10000++
         }
     }
 }
