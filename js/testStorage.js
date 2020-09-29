651a426044ffb4e7ca8976e3e629377e345c555d
Celine Galy <celinegaly63@gmail.com>
2020-09-29 21:27:03 +0200

    maj  fichier storage

--- js/testStorage.js
+++ js/testStorage.js
@@ -9,22 +9,27 @@
     let storedIconeSon = localStorage.getItem('iconeSon');
     let storedIconeMute = localStorage.getItem('iconeMute');
     let storedPlayerAudio = localStorage.getItem('playerAudio');
-   // let playerAudioGame = document.getElementById('audioGame');
+   
+    let playerAudioGame = storedPlayerAudio;
     let iconeSonGame = document.getElementById('play');
     let iconeMuteGame = document.getElementById('muted');
 
 
 
 iconeSonGame.addEventListener('click',()=>{
- // playerAudioGame.muted = true;
   iconeMuteGame.classList.remove('hidden');
   iconeMuteGame.classList.add('sound');
   iconeSonGame.classList.add('hidden');
 
 })
-iconeMuteGame.addEventListener('click',()=>{
-  playerAudioGame.muted = false;
+  iconeMuteGame.addEventListener('click',()=>{
   iconeSonGame.classList.remove('hidden');
   iconeSonGame.classList.add('sound');
   iconeMuteGame.classList.add('hidden');
+})
+iconeSon.addEventListener('click',()=>{
+  storedPlayerAudio.muted = true;
+})
+iconeMute.addEventListener('click',()=>{
+  storedPlayerAudio.muted = false;
 })
