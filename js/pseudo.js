a9b2af36212d8ea7b62f3bd6ed1a73516ca263be
Celine Galy <celinegaly63@gmail.com>
2020-09-29 10:10:02 +0200

    modif loggin

--- js/pseudo.js
+++ js/pseudo.js
@@ -1,32 +1,63 @@
-let btnLogin = document.getElementById('btnMenu'); 
-let pseudoPlayerTwo = document.getElementById('pseudoPlayerTwo').value;
+let btnLogin = document.getElementById('btnMenu');
 let score = document.getElementById('score');
+let pseudoPlayerOne = document.getElementById('pseudoInscription').value;
 
-btnLogin.addEventListener('click',()=>{
-    let pseudoPlayerOne = document.getElementById('pseudoPlayerOne').value;
-    console.log("ok");
-    validateForm();
-    // Afficher la valeur
-    console.log(pseudoPlayerOne);
-    document.getElementById('PlayerOneDuo').value = pseudoPlayerOne;
+function setUserPseudoPlayerOne() {
+  pseudoPlayerOne = localStorage.setItem('pseudo', pseudoPlayerOne);
+  console.log("test local");
+}
+
+btnLogin.addEventListener('click', () => {
+  console.log("ok");
+  validateForm();
+  setUserPseudoPlayerOne();
+  console.log("taratata");
+
 });
 
-    
-let pseudoPlayerOne = localStorage.setItem('pseudoPlayerOne','');
-
-
 function validateForm() {
-    var x = document.forms["logginForm"]["pseudo"].value;
-    if (x == "") {
-      alert("Veuillez indiquer un pseudo pour jouer!");
-      return false; 
-    }
-    else {
-      sectionMenu.classList.remove('hidden');
-        sectionOption.classList.add('hidden');
-        sectionScore.classList.add('hidden');
-        sectionPseudo.classList.add('hidden');
-        //sectionMulti.classList.add('hidden');
-        console.log('Click Btn Menu');
-    }
-  } 
+  let x = document.forms["logginForm"]["pseudo"].value;
+  console.log(x);
+  console.log("test");
+  if (x == "") {
+    alert("Veuillez indiquer un pseudo pour jouer!");
+    return false;
+  }
+  else {
+    sectionMenu.classList.remove('hidden');
+    sectionOption.classList.add('hidden');
+    sectionScore.classList.add('hidden');
+    sectionPseudo.classList.add('hidden');
+    sectionMulti.classList.add('hidden');
+    
+    document.getElementById('PlayerOneDuo').value = x;
+    console.log('Click Btn Menu');
+  }
+}
+
+btnJouerDuo.addEventListener('click', () => {
+  validateInput();
+  console.log('Click Btn Jouer Duo');
+})
+
+
+function validateInput() {
+  console.log("ca passe");
+  let pseudoPlayerTwo = document.forms["formDuo"]["pseudo2"].value;
+  
+  if (pseudoPlayerTwo == "") {
+    alert("Veuillez indiquer un pseudo pour jouer!");
+    return false;
+  }
+  else {
+    setUserPseudoPlayerTwo();
+    console.log(pseudoPlayerTwo);
+   window.location = "modeDuo.html";
+
+}}
+
+function setUserPseudoPlayerTwo() {
+  pseudoPlayerTwo = localStorage.setItem('pseudo', pseudoPlayerTwo);
+  
+  console.log(pseudoPlayerTwo.pseudo);
+}
