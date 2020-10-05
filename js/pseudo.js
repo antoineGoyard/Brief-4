b4fcd042d7a77328c22a1827eab041ed7fc3d2ad
celine galy <celinegaly63@gmail.com>
2020-10-05 10:14:22 +0200

    modifs scripts et css

--- js/pseudo.js
+++ js/pseudo.js
@@ -7,26 +7,30 @@ let inputPseudo = document.getElementById('pseudoInscription');
 
 btnLogin.addEventListener('click', () => {
   console.log("ok");
-  
   pseudoPlayerOne = document.getElementById('pseudoInscription').value;
   localStorage.setItem('pseudoPlayerOne', pseudoPlayerOne);
-validateForm();
+  validateForm();
 });
 
-// ifinputPseudo.addEventListener('keypress',()=>{
-//   var e=window.event || e;
-//   var touche=e.charCode || e.keyCode;
-// if(touche==13){
-//   pseudoPlayerOne = document.getElementById('pseudoInscription').value;
-//   localStorage.setItem('pseudoPlayerOne', pseudoPlayerOne); 
-//     console.log("ca marche");
-//     sectionMenu.classList.remove('hidden');
-//     sectionPseudo.classList.add('hidden');
-//     sectionOption.classList.add('hidden');
-//     sectionScore.classList.add('hidden');
-//     sectionMulti.classList.add('hidden');
-//         console.log('Click Btn Menu');
-// }})
+inputPseudo.addEventListener('keypress', () => {
+
+  var e = window.event || e;
+  var touche = e.charCode || e.keyCode;
+  if (touche == 13) {
+    e.preventDefault();
+
+    pseudoPlayerOne = document.getElementById('pseudoInscription').value;
+    localStorage.setItem('pseudoPlayerOne', pseudoPlayerOne);
+    console.log("ca marche");
+    validateForm();
+    // sectionMenu.classList.remove('hidden');
+    // sectionPseudo.classList.add('hidden');
+    // sectionOption.classList.add('hidden');
+    // sectionScore.classList.add('hidden');
+    // sectionMulti.classList.add('hidden');
+    console.log('Click Btn Menu');
+  }
+})
 
 function validateForm() {
   let x = document.forms["logginForm"]["pseudo"].value;
@@ -48,26 +52,31 @@ function validateForm() {
 }
 
 btnJouerDuo.addEventListener('click', () => {
-  validateFormDuo();
+  
   console.log('Click Btn Jouer Duo');
   let pseudoPlayerTwo = document.getElementById('pseudoPlayerTwo').value;
   localStorage.setItem('pseudoPlayerTwo', pseudoPlayerTwo);
   console.log("adjugé");
+  validateFormDuo();
 })
 
 
 function validateFormDuo() {
   console.log("ca passe");
-  let y = document.forms["formDuo"]["pseudo"].value;
+  let y = document.forms["formDuo"]["pseudo2"].value;
   if (y == "") {
     alert("Veuillez indiquer un pseudo pour jouer!");
     return false;
   }
+   if (x == y){
+     alert("Veuillez indiquer un autre pseudo!");
+     return false;
+   }
   else {
-    
+
     console.log(pseudoPlayerTwo);
+    console.log(pseudoPlayerTwo);
+    window.location = "modeDuo.html";
 
-  console.log(pseudoPlayerTwo);
-   window.location = "modeDuo.html";
-
-}}
+  }
+}
--- js/video.js
+++ js/video.js
@@ -2,25 +2,28 @@
 let player = document.getElementById('player');
 let soundOn = document.getElementById('soundOn');
 let soundOff = document.getElementById('soundOff');
+let btnSkip = document.getElementById('btnSkip');
 
 soundOn.addEventListener('click',()=>{
-    player.play()
+    btnSkip.classList.remove('hidden');
+    player.play();
     document.getElementById('modale').classList.add('hidden');
     setTimeout(function(){
     console.log("Hello");
-    document.getElementById('btnSkip').click();
-}, 10000);
+    btnSkip.click();
+}, 12000);
 
 });
 
 soundOff.addEventListener('click',()=>{
+    btnSkip.classList.remove('hidden');
     player.play();
     player.muted = "true";
     document.getElementById('modale').classList.add('hidden');
     setTimeout(function(){
         console.log("Hello");
-        document.getElementById('btnSkip').click();
-    }, 10000);
+        btnSkip.click();
+    }, 12000);
 });
 
 
