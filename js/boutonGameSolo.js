15cd467f978715cb5e44ac899dddcd4970177e72
Celine Galy <celinegaly63@gmail.com>
2020-10-07 18:53:06 +0200

    gestion son video game solo

--- js/boutonGameSolo.js
+++ js/boutonGameSolo.js
@@ -1,6 +1,9 @@
 let btnMenuVictoireSolo = document.getElementById('btnMenuVictoireSolo');
 let btnValidation = document.getElementById('btn_btn1');
 let pseudoPlayerOne = localStorage.getItem("pseudoPlayerOne");
+let videoExplication = document.getElementById('video');
+let sonExplication = document.getElementById('playExplication');
+let muteExplication = document.getElementById('mutedExplication');
 
 btnMenuVictoireSolo.addEventListener('click',()=>{
     window.location= "page2.html";
@@ -9,4 +12,19 @@ btnMenuVictoireSolo.addEventListener('click',()=>{
 
 btnValidation.addEventListener('click',()=>{
     console.log(pseudoPlayerOne);
+    
+})
+sonExplication.addEventListener('click',()=>{
+    videoExplication.muted="true";
+    sonExplication.classList.add('hidden');
+    muteExplication.classList.remove('hidden');
+    muteExplication.classList.add('sound');
+   
+})
+muteExplication.addEventListener('click',()=>{
+    videoExplication.muted= false;
+    muteExplication.classList.add('hidden');
+    sonExplication.classList.remove('hidden');
+    sonExplication.classList.add('sound');
+    
 })
